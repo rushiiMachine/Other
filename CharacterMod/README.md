14b818a3cb718496af2dd82db24c94a1e996bbe7
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-01-19 13:07:30 -0800

    Remove disgusting CharacterMod

--- CharacterMod/README.md
+++
@@ -1,6 +0,0 @@
-# Character Mod
-A mod that adds placeable letters and other characters in diffrent styles!
-
-Minecraft Forge 1.12.2
-
-Liscenced under MMPL v2.03
--- CharacterMod/src/main/java/tk/diamondbuildz/mod/character/server/ServerUtil.java
+++
@@ -1,9 +0,0 @@
-package tk.diamondbuildz.mod.character.server;
-
-/**
- * Util that is only used on the Physical/Dedicated Server
- *
- * @author Cadiboo
- */
-public final class ServerUtil {
-}
